2a93b12b974564ca4e39287b5212813e9207f1ba
cam-ge <60639682+cam-ge@users.noreply.github.com>
2021-05-03 14:32:16 -0700

    Debugged edition of main.js

--- main.js
+++ main.js
@@ -3,18 +3,16 @@ var myargs = process.argv.slice(2);
 myargs.forEach(convert)
 
 
-function convert(value,index,array){
-    var digits = ['Zero','One','Two','Three','Four',
+function convert(value){
+    let digits = ['Zero','One','Two','Three','Four',
         'Five','Six','Seven','Eight','Nine'];
-    var num = parseInt(value);
-    var words = '';
-    var counter = 0 ;
+    let words = '';
+    let counter = 0 ;
     while(counter<value.length){
         words+= digits[value[counter]];
         counter ++;
     }
     txt =  txt + words+ ',';
-
 }
 txt = txt.substring(0,txt.length-1);
 console.log(txt);
